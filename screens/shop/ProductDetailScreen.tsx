c021ada2f3e8fbd58d2a6aa8a65b7aba235c6873
Amir Muhammad Hakim <amir.muh.hakim@gmail.com>
2021-02-14 11:35:33 +0700

    ✨ Add product detail screen

--- screens/shop/ProductDetailScreen.tsx
+++ screens/shop/ProductDetailScreen.tsx
@@ -1,18 +1,13 @@
 import React, { FC, useLayoutEffect } from "react";
 import { useNavigation, useRoute } from "@react-navigation/native";
-import {
-  View,
-  Text,
-  // ScrollView,
-  // Image,
-  // Button,
-  // StyleSheet,
-} from "react-native";
+import { View, ScrollView, Image, Button, StyleSheet } from "react-native";
 
 import {
   ProductDetailScreenNavProp,
   ProductDetailScreenRouteProp,
 } from "../../navigation/ProductsStack/types";
+import BodyText from "../../components/text/BodyText";
+import Colors from "../../constants/colors";
 
 const ProductDetailScreen: FC = () => {
   const navigation = useNavigation<ProductDetailScreenNavProp>();
@@ -25,10 +20,45 @@ const ProductDetailScreen: FC = () => {
   }, [navigation, params.product.title]);
 
   return (
-    <View>
-      <Text>{params.product.title}</Text>
-    </View>
+    <ScrollView>
+      <Image style={styles.image} source={{ uri: params.product.imageUrl }} />
+      <View style={styles.action}>
+        <Button
+          title="ADD TO CART"
+          color={Colors.Primary}
+          onPress={() => null}
+        />
+      </View>
+      <BodyText style={styles.price}>
+        ${params.product.price.toFixed(2)}
+      </BodyText>
+      <BodyText style={styles.description}>
+        {params.product.description}
+      </BodyText>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: 300,
+  },
+  action: {
+    marginTop: 20,
+    alignItems: "center",
+  },
+  price: {
+    fontSize: 20,
+    color: "#888",
+    textAlign: "center",
+    marginVertical: 20,
+  },
+  description: {
+    fontSize: 14,
+    textAlign: "center",
+    marginHorizontal: 20,
+  },
+});
 
 export default ProductDetailScreen;
